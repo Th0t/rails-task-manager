86b10bb74d8662e8911cfb48b6c1228b5e578554
Julien Vanderstock <julien.vanderstock@openmailbox.org>
2017-07-11 16:08:06 +0200

    weird things happened, back to normal but from another version that doesn't exist anymore

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'task/index'
+  get 'tasks',          to: "tasks#index"
 
-  get 'task/show'
+  get 'tasks/new',      to: "tasks#new",      as: "new_task"
+  post 'tasks',         to: "tasks#create"
 
-  get 'task/new'
+  get 'tasks/:id',      to: "tasks#show",     as: "task"
 
-  get 'task/create'
+  get 'tasks/:id/edit', to: "tasks#edit",     as: "edit_task"
+  patch 'tasks/:id',    to: "tasks#update"
 
-  get 'task/edit'
+  delete 'tasks/:id',   to: "tasks#destroy"
 
-  get 'task/update'
-
-  get 'task/destroy'
+  # 7 lines before can be sumerize in that way:?
+  # resources:          :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
