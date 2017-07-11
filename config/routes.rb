5ab08d50f6c64855af7115e68d6a9b1d5e30b219
Julien Vanderstock <julien.vanderstock@openmailbox.org>
2017-07-11 14:28:03 +0200

    generated TaskController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'task/index'
+
+  get 'task/show'
+
+  get 'task/new'
+
+  get 'task/create'
+
+  get 'task/edit'
+
+  get 'task/update'
+
+  get 'task/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
